ed05055158c51f415942085a98ebde58d8b3410c
lizzethgd <lizzethgd@gmail.com>
2021-08-03 11:34:54 +0300

    deploy 13

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static('public'))
 
-app.use(cors());
 app.use(express.json())
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '/client/build')));
 
+app.use(cors());
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
