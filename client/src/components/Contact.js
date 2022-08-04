d4c528b9301f0dc5a24bbeb2b07b4b3c39a821a0
lizzethgd <lizzethgd@gmail.com>
2022-08-04 14:59:26 +0300

    contact parallax updated

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,4 +1,5 @@
 import Helsinki from "../assets/images/hel02.png";
+//import "../assets/css/parallax.scss";
 import {useState} from 'react'
 import {sendMail} from '../services/ContactService'
 import { useTranslation} from 'react-i18next';
@@ -30,17 +31,17 @@ const Contact = () => {
     //setContact_me(false)
   }
 
-  const Parallax = {
-      backgroundAttachment: 'fixed',
-      backgroundPosition: 'center',
+ const Parallax = {
+      backgroundAttachment: 'fixed'
+      /*  backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-  }
+      backgroundSize: 'cover',*/
+  } 
     
   const { t } = useTranslation("global");
 return (
 <section id="contact" className="w3-light-grey" >
-  <div className="bgimg-3 w3-display-container " style={Parallax}>
+  <div className="bgimg-3 w3-display-container " style={Parallax} >
     <div className="w3-display-middle">
       <span className="w3-xxlarge w3-text-white w3-wide">{t("nav.contact")}</span>
     </div>
